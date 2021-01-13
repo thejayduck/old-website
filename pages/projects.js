20b8993bbe0f57eaaeb403d57ff8bcd8c2521ac5
TheJayDuck <therealjayduck@gmail.com>
2021-01-14 00:39:10 +0300

    Fixes width scale

--- pages/projects.js
+++ pages/projects.js
@@ -3,30 +3,30 @@ import { getGameListData } from '../data'
 import styles from '../styles/Projects.module.css'
 import Navbar from "./navbar"
 
-export default function Projects({gameList}){
-    return(
+export default function Projects({ gameList }) {
+    return (
         <div>
             <Head>
                 <title>TheJayDuck - PROJECTS</title>
                 <meta property="og:description" content="Projects Page" />
             </Head>
-            <Navbar/>
+            <Navbar />
             <div className={styles.projectContent}>
                 {
-                    gameList.map (q =>
+                    gameList.map(q =>
                         q !== undefined ? (
                             <div className={styles.contentElement}>
                                 <h1>{q.title}</h1>
-                                <iframe frameborder="0" src={`https://itch.io/embed/${q.itchID}?border_width=0&bg_color=000000&fg_color=ffffff&link_color=9D00DA&border_color=000000`}></iframe>
+                                <iframe frameborder="0" src={`https://itch.io/embed/${q.itchID}?bg_color=000000&fg_color=ffffff&link_color=9D00DA&border_color=000000`}></iframe>
                                 <div className={styles.about}>
-                                    About<hr/>{q.about}
+                                    About<hr />{q.about}
                                 </div>
                                 <div className={styles.gameGenre}>
                                     {q.genre}
                                 </div>
                             </div>
                         ) : <p> Nothing to See Here </p>
-                    )   
+                    )
                 }
             </div>
         </div>
@@ -36,7 +36,7 @@ export default function Projects({gameList}){
 export async function getStaticProps({ params }) {
     return {
         props: {
-          gameList: getGameListData(),
+            gameList: getGameListData(),
         }
     }
-  }
+}
