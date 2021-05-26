1e0258108e88aca41ad22a6abf3ea8a4211e4d43
thejayduck <therealjayduck@gmail.com>
2021-05-26 17:34:05 +0300

    Improves drawings page

--- pages/components/drawingData.js
+++ pages/components/drawingData.js
@@ -5,13 +5,13 @@ export default function DrawingData({ data }) {
     <div className={styles.column}>
         {
             data.map (q =>
-                q !== undefined ? (
+                q !== null ? (
                     <div className={styles.contentElement}>
                         <div className={styles.image}>
                             {/* <h2>{q.title}</h2> */}
                             {q.r18 ? (<img className={styles.imageFilter} src={q.url}/>) : (<img src={q.url}/>)}
                         </div>
-                        <div className={styles.tool}>{q.software}<br/>{q.hardware}<br/>{q.resolution}</div>
+                        {/* <div className={styles.tool}>{q.software}<br/>{q.hardware}<br/>{q.resolution}</div> */}
                     </div>
                 ) : <p> Nothing to See Here </p>
             )
--- pages/index.js
+++ pages/index.js
@@ -16,52 +16,52 @@ export async function getStaticProps() {
 }
 
 export default function Home({ data }) {
-  return (
-    <div>
-      <Head>
-          <title>TheJayDuck - ABOUT</title>
-          <meta property="og:description" content="About Page" />
-      </Head>
-      <Navbar/>
-      <div className={`${styles.pageContent} pageContent`}>
+    return (
+        <div>
+            <Head>
+                <title>TheJayDuck - ABOUT</title>
+                <meta property="og:description" content="About Page" />
+            </Head>
+            <Navbar />
+            <div className={`${styles.pageContent} pageContent`}>
 
-        <h1>I am Arda!</h1>
-        <h3>
-            And I'm a programmer, an artist, and a university student. In my free time, I draw original characters and create games/apps.<br/>
+                <h1>I am Arda!</h1>
+                <h3>
+                    And I'm a programmer, an artist, and a university student. In my free time, I draw original characters and create games/apps.<br />
             You can scroll down further to read FAQ.
         </h3>
-        <div className={styles.toolContainer}>
-            <div className={styles.tool}>
-                <h3>Programming Languages</h3>
-                <p>C#, PHP, Rust*</p>
-            </div>
-            <div className={styles.tool}>
-                <h3>Software</h3>
-                <p>
-                    Visual Studio Code<br/>
-                    MediBang Paint Pro (PC)<br/>
-                    Infinite Painter (Android)<br/>
-                    Visual Studio 2019<br/>
-                    Unity Engine<br/>
+                <div className={styles.toolContainer}>
+                    <div className={styles.tool}>
+                        <h3>Programming Languages</h3>
+                        <p>C#, PHP, Rust*</p>
+                    </div>
+                    <div className={styles.tool}>
+                        <h3>Software</h3>
+                        <p>
+                            Visual Studio Code<br />
+                    MediBang Paint Pro (PC)<br />
+                    Infinite Painter (Android)<br />
+                    Visual Studio 2019<br />
+                    Unity Engine<br />
                     Stride Engine*
                 </p>
-            </div>
-            <div className={styles.tool}>
-                <h3>Hardware</h3>
-                <p>
-                    Custom Built Desktop<br/>
-                    Acer Swift 3 SF314-58G<br/>
-                    Wacom Intuos Draw CTL-490<br/>
+                    </div>
+                    <div className={styles.tool}>
+                        <h3>Hardware</h3>
+                        <p>
+                            Custom Built Desktop<br />
+                    Acer Swift 3 SF314-58G<br />
+                    Wacom Intuos Draw CTL-490<br />
                     Samsung Galaxy Tab S6 Lite
                 </p>
+                    </div>
+                    <hr />
+                    <footer>* is put for software/language I am still learning</footer>
+                </div>
+                <br />
+                <h1>FAQ</h1>
+                <FAQData data={data.faq} />
             </div>
-            <hr/>
-            <footer>* is put for software/language I am still learning</footer>
         </div>
-        <br/>
-        <h1>FAQ</h1>
-        <FAQData data={data.faq}/>
-      </div>
-    </div>
-  )
+    )
 }
