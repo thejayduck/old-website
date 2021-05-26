17d80b19408b8dd5999b00eb2663f49d615b2dd2
thejayduck <therealjayduck@gmail.com>
2021-05-26 17:45:53 +0300

    fixes drawings page

--- pages/components/drawingData.js
+++ pages/components/drawingData.js
@@ -5,15 +5,13 @@ export default function DrawingData({ data }) {
     <div className={styles.column}>
         {
             data.map (q =>
-                q ? (
-                    <div className={styles.contentElement}>
-                        <div className={styles.image}>
-                            {/* <h2>{q.title}</h2> */}
-                            {q.r18 ? (<img className={styles.imageFilter} src={q.url}/>) : (<img src={q.url}/>)}
-                        </div>
-                        {/* <div className={styles.tool}>{q.software}<br/>{q.hardware}<br/>{q.resolution}</div> */}
+                <div className={styles.contentElement}>
+                    <div className={styles.image}>
+                        {/* <h2>{q.title}</h2> */}
+                        {q.r18 ? (<img className={styles.imageFilter} src={q.url}/>) : (<img src={q.url}/>)}
                     </div>
-                ) : <p> Nothing to See Here </p>
+                    {/* <div className={styles.tool}>{q.software}<br/>{q.hardware}<br/>{q.resolution}</div> */}
+                </div>
             )
         }
     </div>
